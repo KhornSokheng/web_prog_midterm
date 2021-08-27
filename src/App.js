26680ff113081784d37ef7b78ff121cc5e1ba1e1
Mujalin <62310021@my.buu.ac.th>
2021-08-27 23:10:07 +0700

    add importcomponents

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import Report from './components/Report';
+import Home from './components/Home';
+import contectus from './components/contectus';
+import skill from './components/skill';
+import aboutus from './components/aboutus';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import profile from './components/profile';
 function App() {
   return (
     <div className="App">
