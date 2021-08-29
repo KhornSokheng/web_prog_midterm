4bcf91e6fad3d8960e6d56689e707f03c2ffe33b
Suchapong <59908364+Suchapong@users.noreply.github.com>
2021-08-30 00:27:50 +0700

    change contectus

--- src/components/contectus.js
+++ src/components/contectus.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 export default function contectus() {
     return (
         <div>
-            <h1></h1>
+            <h1>Contectus</h1>
+            <p>email</p>
+            <input type ="text" id="fname" name="fname">
+            </input>
         </div>
     )
 }
