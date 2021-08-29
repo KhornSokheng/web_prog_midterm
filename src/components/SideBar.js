0f54b1fa970106e1ac00b90f4bbb44d6702f7254
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2021-08-29 22:07:13 +0700

    add logo in /public/

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,8 +7,8 @@ export default function SideBar() {
             <aside className="main-sidebar sidebar-dark-primary elevation-4">
                 {/* Brand Logo */}
                 <a href="index3.html" className="brand-link">
-                    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-                    <span className="brand-text font-weight-light">AdminLTE 3</span>
+                    <img src="g2-logo2.png" alt="Group2 Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8'}} />
+                    <span className="brand-text font-weight-light">Group 2</span>
                 </a>
                 {/* Sidebar */}
                 <div className="sidebar">
