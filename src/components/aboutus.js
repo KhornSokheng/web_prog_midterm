552c08321df225afb81d8c3bb6811fa0e35b992a
Lapasrada <59908441+Lapasrada@users.noreply.github.com>
2021-09-03 14:19:17 +0700

    update about us

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,93 +1,67 @@
-import React from 'react'
-import '../css/global.css'
-import '../css/aboutstyle.css'
+import React from "react";
+import "../css/global.css";
+import "../css/aboutstyle.css";
 
 export default function aboutus() {
-    return (
+  return (
+    <div className="content-wrapper">
+      <div className="about-section">
+        <h1>About Us </h1>
+      </div>
 
-        <div className="content-wrapper">
-            <div className ="container-fluid">
-            <h1 className ="abus">About Us</h1>
-
+      <div className="bg">
+        <div className="row">
+          
+          <div className="card" style={{ width: "19rem" }}>
+            <img className="card-img-top" src="/dist/img/goup/mu.jpg" alt="Card image cap" />
+            <div className="card-body">
+              <h1 className="card-title">Mujalin Khattiyanon (Mulin)</h1>
+              <br/>
+              <p className="card-text">CEO</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          
+          <div className="card" style={{ width: "19rem" }}>
+            <img className="card-img-top" src="/dist/img/goup/non.jpg" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Khorn Sokheng (Non)</h5>
+              <br/>
+              <p className="card-text">Art Director</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          
+          <div className="card" style={{ width: "19rem" }}>
+            <img className="card-img-top" src="/dist/img/goup/aum.jpg" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Suchapong Songprakon (Aum)</h5>
+              <br/>
+              <p className="card-text">Designer</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          
+          <div className="card" style={{ width: "19rem" }}>
+            <img className="card-img-top" src="/dist/img/goup/oui.jpg" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Lapusrada Jonboonruang (Oui)</h5>
+              <br/>
+              <p className="card-text">Unemployed</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
         
-        <div className="box01">
-            <img className="imgab" src="dist/img/goup/mu.jpg" width="100%"/>
-            <h1 className="textab">Name : Mujalin Khattiyanon
-            <br/>Nickname : Mulin 
-            <br/>Brithday : 28 April
-            </h1>
-        </div>
-       
-        <div className="box02">
-            <img src="dist/img/goup/oui.jpg"></img>
-            <h1 className="textab">Name : Khorn Sokheng 
-            <br/>Nickname : Non 
-            <br/>Brithday : 
-            </h1>
-        </div>
-
-        </div>
-            <div className = "box03">
-            <img src="dist/img/goup/oui.jpg"></img>
-            <h1 className="textab">Name : Lapusrada Jonboonruang 
-            <br/>Nickname : Oui 
-            <br/>Brithday : 2 Jun
-            </h1>
-        </div>
-
-        <div class="box04">
-            <img src="dist/img/goup/Aum.jpg"></img>
-            <h1  className="textab">Name : Suchapong Songprakon 
-            <br/>Nickname : Aum 
-            <br/>Brithday : 25 Nov
-            </h1>
-        </div>
-
-        <script src="public/js/jquery.js"></script>
-        <script src="public/js/slick.min.js"></script>
-
-        <section className="aboutbox">
-            <div class = "about">
-                <h1>About us</h1>
-            </div>   
-          <div className="container">
-            {/* Info boxes */}
-            < div className="row">
-              <div className="box">
-                <div className="box">
-                  <img src="dist/img/goup/mu.png" BORDER ="3"  ></img> 
-                  <h2>Mujalin Khattiyanon</h2>
-                  <h3>Information Technology</h3>
-                </div> 
-                </div>
-                <div className="box2">
-                <div className="box">
-                  <img src="dist/img/goup/non.png" BORDER ="3" ></img>
-                    <h2>Khorn Sokheng</h2>
-                    <h3>Information Technology</h3>
-                </div> 
-                </div>
-                <div className="box3">
-                <div className="box">
-                  <img src="dist/img/goup/Ou.png" ></img> 
-                  <br/>
-                   Lapusrada Jonboonruang 
-                   <h3> Information Technology </h3>
-                </div> 
-            </div>
-                <div className="box4">
-                <div className="box">
-                  <img src="dist/img/goup/aum.png" BORDER ="3"  ></img> 
-                  <h2> Suchapong Songprakon </h2>
-                  <h3> Information Technology </h3>
-                </div>
-            </div>
-            </div> 
-            </div>
-           
-        </section>
-        </div>
-
-        
-    )
+      </div>
+    </div>
+    </div>
+  );
 }
--- src/components/skill.js
+++ src/components/skill.js
@@ -1,151 +1,153 @@
-import React from 'react'
-import '../css/skillstyle.css'
+import React from "react";
+import "../css/skillstyle.css";
 export default function skill() {
-    return (
-          <div className="container-fluid">
-            {/* Info boxes */}
-            < div className="row">
-              <div className="box">
-                <div className="box5">
-                  <img src="dist/img/goup/mu.jpg" BORDER ="3"  ></img> 
-                       Mujalin Khattiyanon
-                  <div className= "lau">
-                 <h3>Skill</h3>
-                  <h7>Javascript:
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                  </h7>
-                   
-                    <h4>Pyton : 
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h4>
-                    <h5>Html : 
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h5>
-                     
-                    <h6>CPP : 
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h6>
-                </div> 
-                </div> 
-                </div>
-                <div className="box2">
-                <div className="box6">
-                  <img src="dist/img/goup/non.jpg" BORDER ="3" ></img> 
-                   Khorn Sokheng
-                  <div className= "lau"> 
-                  <h3>Skill</h3>
-                  <h7>
+  return (
+    <div className="content-wrapper">
+      <div className="container-fluid">
+        {/* Info boxes */}
+        <div className="row">
+          <div className="box">
+            <div className="box5">
+              <img src="dist/img/goup/mu.jpg" BORDER="3"></img>
+              Mujalin Khattiyanon
+              <div className="lau">
+                <h3>Skill</h3>
+                <h7>
                   Javascript:
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h7>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h7>
 
-                    <h4>Pyton :  <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h4>
+                <h4>
+                  Pyton :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h4>
+                <h5>
+                  Html :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h5>
 
-                    <h5>Html :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                     </h5>
-                        
-                    <h6>CPP :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                     </h6>
-                        
-                    </div>
-                </div> 
-                </div>
-                <div className="box3">
-                <div className="box">
-                  <img src="dist/img/goup/oui.jpg" BORDER ="3"width="370" height="370" ></img> 
-                
-                 Lapusrada Jonboonruang 
-                  <div className= "lau"> 
-                  <h3>Skill</h3>
-                  <h7>
-                      
-                  Javascript: 
+                <h6>
+                  CPP :<i class="fas fa-star"></i>
                   <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    
-                  </h7>
-                 
-                    <h4>Pyton :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    
-                    </h4>
-                        
-                    <h5>Html :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                     </h5>
-                        
-                    <h6>CPP :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                     </h6>
-                
-                    </div>
-                </div> 
+                </h6>
+              </div>
             </div>
-                <div className="box4">
-                <div className="box">
-                  <img src="dist/img/goup/aum.jpg" BORDER ="3"  ></img> 
-                  Suchapong Songprakon 
-                  <div className= "lau">
-                  <h3>Skill</h3> 
-                  <h7>
-                      Javascript:  
+          </div>
+          <div className="box2">
+            <div className="box6">
+              <img src="dist/img/goup/non.jpg" BORDER="3"></img>
+              Khorn Sokheng
+              <div className="lau">
+                <h3>Skill</h3>
+                <h7>
+                  Javascript:
                   <i class="fas fa-star"></i>
                   <i class="fas fa-star"></i>
-                  </h7>
-                    <h4>Pyton :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                     </h4>
-                        
-                    <h5>Html :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h5>
-                        
-                    <h6>CPP :
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    </h6>
-                      
-                    </div>
-                </div>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h7>
+
+                <h4>
+                  Pyton : <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h4>
+
+                <h5>
+                  Html :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h5>
+
+                <h6>
+                  CPP :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h6>
+              </div>
             </div>
-            </div> 
+          </div>
+          <div className="box3">
+            <div className="box">
+              <img
+                src="dist/img/goup/oui.jpg"
+                BORDER="3"
+                width="370"
+                height="370"
+              ></img>
+              Lapusrada Jonboonruang
+              <div className="lau">
+                <h3>Skill</h3>
+                <h7>
+                  Javascript:
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h7>
+
+                <h4>
+                  Pyton :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h4>
+
+                <h5>
+                  Html :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h5>
+
+                <h6>
+                  CPP :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h6>
+              </div>
             </div>
-    )
+          </div>
+          <div className="box4">
+            <div className="box">
+              <img src="dist/img/goup/aum.jpg" BORDER="3"></img>
+              Suchapong Songprakon
+              <div className="lau">
+                <h3>Skill</h3>
+                <h7>
+                  Javascript:
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h7>
+                <h4>
+                  Pyton :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h4>
+
+                <h5>
+                  Html :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h5>
+
+                <h6>
+                  CPP :<i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
